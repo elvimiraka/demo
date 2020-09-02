9f692f2eba73221c1e80dae630061d131e3c08a2
elvimiraka <elvi7miraka@gmail.com>
2020-09-02 19:52:48 +0200

    Updates

---
+++ precache-manifest.399145599cb434ab0c443cc9060f78d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6494903100e844fca5ee17263b6b4d",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "c41078ff53e81ae15aed",
+    "url": "/demo/static/css/main.944ceaa4.chunk.css"
+  },
+  {
+    "revision": "a16ffe78ca1abe8e2f1d",
+    "url": "/demo/static/js/2.2e8c0c4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.2e8c0c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c41078ff53e81ae15aed",
+    "url": "/demo/static/js/main.47bc56d0.chunk.js"
+  },
+  {
+    "revision": "b39b67791598b779560b",
+    "url": "/demo/static/js/runtime-main.a819b733.js"
+  },
+  {
+    "revision": "6d71fdf016df9b78128989e1aacdd104",
+    "url": "/demo/static/media/logo.6d71fdf0.jpg"
+  }
+]);
